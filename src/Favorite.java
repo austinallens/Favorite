5a5ac5459021a819941ca418d9ed6f6a625ed199
Austin Allen <95332894+austinallens@users.noreply.github.com>
2023-07-30 01:29:16 -0500

    Add files via upload

---
+++ src/Favorite.java
@@ -0,0 +1,15 @@
+/* Name: Austin Allen
+ * Course: CSC 135
+ * Project: Problem 1.1
+ * File Name: Favorite.java
+ */
+
+public class Favorite {
+
+    public static void main(String[] args) {
+        System.out.println("United States:\nStatue of Liberty and Raising "
+                + "Canes");
+
+    }
+    
+}
